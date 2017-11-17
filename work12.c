47c6eb4499c16e1ccb79167cf17fb0117915a79b
Bayan Berri <bberri@stuy.edu>
2017-11-16 20:14:41 -0500

    im not sure

--- work12.c
+++ work12.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include <signal.h>
-#include <string.h>
 #include <time.h>
-
+#include <sys/wait.h>
 int main(){
   printf("pp id b4: %d\n",getpid());
-  fork();
-  fork();
-  printf("pid after:%d\n", getpid());
-  srand(time(NULL));
+  int frk=fork();
+  if(frk==0){
+    printf("pid of child:%d\n", getpid());
+    srand(time(NULL));
+    int s= rand() % 16 + 5;//rand() % (max +1 -min) + min
+    sleep(s);
+    printf("pid: %d is finished after %d seconds\n",getpid(),s);
+    exit(0);
+  }
+  if(frk){
+    int i;
+    int pid= wait( &i );
+    printf("status: %d\n", pid);
+  }
 
-  int s= rand() % 16 + 5;//rand() % (max +1 -min) + min
-  sleep(s);
 
-  printf("pid: %d is finished\n",getpid());
-  int i;
-
-  int pid=wait( &i );
-  printf("status: %d\n", pid,s);
+  return 0;
 
 }
